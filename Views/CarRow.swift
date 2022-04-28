c1ee83a96bbb7c813866fba40389e08d44b0ecb3
Oncu <oohancan@gmail.com>
2022-04-28 15:21:19 +0300

    add pic, text and price

--- Views/CarRow.swift
+++ Views/CarRow.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct CarRow: View {
     var body: some View {
+        VStack (alignment: .leading) {
         Image("teslax")
             .resizable()
             .aspectRatio(contentMode: .fit)
+        
+            HStack {
+                Text("Tesla Model X")
+                    .font(.title)
+                .fontWeight(.semibold)
+                Spacer()
+                Text("$90,000")
+                    .font(.headline)
+            }
+            .padding(.trailing, 10)
+            .padding(.leading, 10)
+                
+        }
     }
 }
 
