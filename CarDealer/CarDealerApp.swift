1a721f6742a78ffa1f51b907f367868d804c17f4
Oncu <oohancan@gmail.com>
2022-04-28 14:50:49 +0300

    Initial Commit

---
+++ CarDealer/CarDealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealerApp.swift
+//  CarDealer
+//
+//  Created by Oncu Ohancan on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CarDealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
